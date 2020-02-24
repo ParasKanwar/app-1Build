85183450edc05904f8761eaa4c159677fdd66db9
paras <paraskanwar30@gmail.com>
2020-02-24 21:44:45 +0530

    hello world

---
+++ precache-manifest.298c559e78744f16ad835e8e0d5b6266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689af09cbf83f3ebcc87192ca8544c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679b6973a6e104df3387",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "155f137a34725d26e5bf",
+    "url": "/static/js/2.69a82814.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.69a82814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679b6973a6e104df3387",
+    "url": "/static/js/main.7985af49.chunk.js"
+  },
+  {
+    "revision": "54a289e07caccddbf009",
+    "url": "/static/js/runtime-main.b8947b2b.js"
+  },
+  {
+    "revision": "bc7f0fe661388010b3c9c80d282649d6",
+    "url": "/static/media/complainify.bc7f0fe6.png"
+  }
+]);
